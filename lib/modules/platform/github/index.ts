d13a2ad277183f47b6cd03ccc1a6c94c173b6250
Jamie Tanna <jamie.tanna@mend.io>
2026-02-10 10:55:40 +0000

    chore(github): warn when using default Git author on GitHub.com
    As part of #39309 we enabled "Vigilant Mode" for the `@renovate-bot`
    author.
    
    To give an indication to folks who are self-hosting (on GitHub.com) and
    using this Git commit author, we can provide a log message to let them
    know.
    
    This does not at all stop usage of this default committer email address,
    but does provide a "nudge" to folks that they may see `Unverified`
    commits pop up.

--- lib/modules/platform/github/index.ts
+++ lib/modules/platform/github/index.ts
@@ -26,6 +26,7 @@ import { isGithubFineGrainedPersonalAccessToken } from '../../../util/check-toke
 import { coerceToNull } from '../../../util/coerce.ts';
 import { parseJson } from '../../../util/common.ts';
 import { getEnv } from '../../../util/env.ts';
+import { parseGitAuthor } from '../../../util/git/author.ts';
 import * as git from '../../../util/git/index.ts';
 import {
   listCommitTree,
@@ -222,6 +223,9 @@ export async function initPlatform({
     renovateUsername,
     token,
   };
+
+  warnIfDefaultGitAuthorEmail(platformResult.gitAuthor, platformConfig.isGhe);
+
   if (
     getEnv().RENOVATE_X_GITHUB_HOST_RULES &&
     platformResult.endpoint === 'https://api.github.com/'
@@ -2190,4 +2194,25 @@ export async function commitFiles(
   await git.resetToCommit(commitResult.parentCommitSha);
   const commitSha = await git.fetchBranch(branchName);
   return commitSha;
+}
+
+function warnIfDefaultGitAuthorEmail(
+  gitAuthor: string | undefined,
+  isGHE: boolean | undefined,
+): void {
+  if (isGHE === true) {
+    return;
+  }
+  const parsed = parseGitAuthor(
+    gitAuthor ?? 'renovate@whitesourcesoftware.com',
+  );
+  if (parsed?.address === 'renovate@whitesourcesoftware.com') {
+    logger.once.warn(
+      {
+        documentationUrl:
+          'https://github.com/renovatebot/renovate/discussions/39309',
+      },
+      'Using the default gitAuthor email address, renovate@whitesourcesoftware.com, is not recommended on GitHub.com, as this corresponds to a user owned by Mend and used by users of the forking-renovate[bot] GitHub App. For security and authenticity reasons, Mend enables "Vigilant Mode" on this account to visibly flag unsigned commits. As an account you do not control, you will not be able to sign commits. If you are comfortable with the `Unverified` signatures on each commit, no work is needed. Otherwise, it is recommended to migrate to a user account you own',
+    );
+  }
 }
